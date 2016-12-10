e6a756728e0aacb00bae847321117ed960a634a2
satuevsky <satuevsky@gmail.com>
2016-12-10 04:36:22 +0400

    getAvgColor added

---
+++ colors/index.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Islam on 09.12.2016.
+ */
+
+module.exports = {
+	getAvgColor: function(canvas){
+		if(!canvas.getImageData) return null;
+		var data = canvas.getImageData(0, 0, canvas.width, canvas.height),
+			r = 0, g = 0, b = 0, a = 0, n = 0;
+		for(var i = 0; i < data.length - 3; i+=4){
+			n++;
+			r += data[i];
+			g += data[i+1];
+			b += data[i+2];
+			a += data[i+3];
+		}
+		return {
+			r: r / n,
+			g: g / n,
+			b: b / n,
+			a: a / n
+		}
+	}
+};
--- index.js
+++ index.js
@@ -2,12 +2,14 @@ var blur = require('./blur');
 var drawText = require('./drawText');
 var drawImage = require('./drawImage');
 var crossDomainImage = require('./lib/crossDomainImage');
+var colors = require('./colors');
 
 module.exports = {
 	blur: blur,
 	drawText: drawText,
 	drawImage: drawImage,
 	crossDomainImage: crossDomainImage,
+	getAvgColor: colors.getAvgColor,
 
 	clear: function(canvas){
 		canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
